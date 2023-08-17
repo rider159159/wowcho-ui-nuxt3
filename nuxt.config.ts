1f174da7642ea735def6e189231940ec30e06d18
Ryder 田圻勳 <ryder159159@staff.1111.com.tw>
2023-08-17 17:25:49 +0800

    測試git

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -34,10 +34,9 @@ export default defineNuxtConfig({
       ],
     }
   },
-  "vite": {
-    "define": {
-      "process.env": process.env,
-    },
-  },
-
+  runtimeConfig: {
+    public: {
+      BASE_API_URL: process.env.BASE_API_URL
+    }
+  }
 })
